a6e3c177993de9c6f4e7449c9a89e94734e692bb
PEDRO HENRIQUE EVERS <pedroevers@gmail.com>
2023-08-03 18:10:25 -0300

    Finalizando validação

--- CalculadoraDePizza.js
+++ CalculadoraDePizza.js
@@ -14,28 +14,55 @@ var inputValor = document.getElementById('inputValor');
 var inputCupom = document.getElementById('inputCupom');
 var inputDesconto = document.getElementById('inputDesconto');
 
-function SalvarPromo(event){
-    event.preventDefault();
-    var pizzaria = inputPizzaria.value;
-    var promocao = inputPromocao.value;
-    var valor = inputValor.value;
-    var medida = inputMedida.value;
-    var tamanho = inputTamanho.value;
-    if (NovaPromo(pizzaria)=false){
-        console.log('teste');
-    }
-    
-}
-function NovaPromo(pizzaria){
+function NovaPromo(pizzaria, promocao, tamanho, valor){
     var Validacao = true
-    if (pizzaria.trim().length === 0){
+    if (pizzaria.trim().length === 0 ){
         inputPizzaria.classList.add('is-invalid');
         Validacao = false;
     }else{
         inputPizzaria.classList.remove('is-invalid');
     }
+
+    if (promocao.trim().length === 0 ){
+        inputPromocao.classList.add('is-invalid');
+        Validacao = false;
+    }else{
+        inputPromocao.classList.remove('is-invalid');
+    }
+
+    if (tamanho.trim().length === 0 || tamanho < 0){
+        inputTamanho.classList.add('is-invalid');
+        Validacao = false;
+    }else{
+        inputTamanho.classList.remove('is-invalid');
+    }
+
+    if (valor.trim().length === 0 || valor < 0){
+        inputValor.classList.add('is-invalid');
+        Validacao = false;
+    }else{
+        inputValor.classList.remove('is-invalid');
+    }
     return Validacao;
 }
+
+function SalvarPromo(event){
+    event.preventDefault();
+    var pizzaria = inputPizzaria.value;
+    var promocao = inputPromocao.value;
+    var tamanho = inputTamanho.value;
+    var frete = inputFrete;
+    var valor = inputValor.value;
+    var medida = inputMedida.value;
+
+    if (NovaPromo(pizzaria, promocao, tamanho, valor) == false){
+        console.log('teste a');
+    }else{
+        console.log('teste b');
+    }
+    return true;
+}
+
 
 
 FormPromo.addEventListener('submit', SalvarPromo);
